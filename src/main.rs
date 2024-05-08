43cb637c8bc9dd5c2075aad79caea70368cafe3b
redcathode <max@b0vik.edv>
2024-05-08 15:34:11 -0400

    basic progress

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ async fn main() {
             if let Ok(server) = response {
                 let tbp = format!("{}: {}", line, server);
                 println!("{}", tbp);
-                eprintln!("\r{:.3}% ({}/{} hosts) - {}", (host_num as f32 / num_hosts as f32) * 100.0, host_num, num_hosts, tbp);
+                // eprintln!("\r{:.3}% ({}/{} hosts) - {}", (host_num as f32 / num_hosts as f32) * 100.0, host_num, num_hosts, tbp);
             }
             
         }).await.unwrap();
